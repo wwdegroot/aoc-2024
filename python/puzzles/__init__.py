14f6f65da53b22be75e7c1baee5c303e90b3a23c
wwdegroot <wouter.w.de.groot@gmail.com>
2024-12-09 21:50:04 +0100

    day 9 in python

--- python/puzzles/__init__.py
+++ python/puzzles/__init__.py
@@ -2,13 +2,10 @@ from pathlib import Path
 
 DATA_DIR = Path(__file__).parent.parent.parent / 'data'
 
-INPUT_ONE = DATA_DIR / 'inputs' / '01.txt'
-INPUT_TWO = DATA_DIR / 'inputs' / '02.txt'
-INPUT_THREE = DATA_DIR / 'inputs' / '03.txt'
-
 INPUTS = {
-    1: INPUT_ONE,
-    2: INPUT_TWO,
-    3: INPUT_THREE,
+    1: DATA_DIR / 'inputs' / '01.txt',
+    2: DATA_DIR / 'inputs' / '02.txt',
+    3: DATA_DIR / 'inputs' / '03.txt',
     4: DATA_DIR / 'inputs' / '04.txt',
+    9: DATA_DIR / 'inputs' / '09.txt',
 }
